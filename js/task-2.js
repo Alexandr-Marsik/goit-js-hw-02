afcce803321c367b115ee83532b922d967eeb1c6
Alexandr <lesiv.sania@gmail.com>
2025-10-26 02:36:37 +0200

    a

--- js/task-2.js
+++ js/task-2.js
@@ -7,17 +7,3 @@
  * - Если длина превышает maxLength, то функция обрезает строку
  * до maxLength символов, добавляет точку "..."
  * в конце и возвращает обрезанную версию. */
-
-function formatMessage(message, maxLength) {
-  if (message.length <= maxLength) {
-    return message;
-  } else {
-    return message.slice(0, maxLength) + '...';
-  }
-}
-console.log(formatMessage('Curabitur ligula sapien', 16)); // "Curabitur ligula..."
-console.log(formatMessage('Curabitur ligula sapien', 23)); // "Curabitur ligula sapien"
-console.log(formatMessage('Vestibulum facilisis purus nec', 20)); // "Vestibulum facilisis..."
-console.log(formatMessage('Vestibulum facilisis purus nec', 30)); // "Vestibulum facilisis purus nec"
-console.log(formatMessage('Nunc sed turpis a felis in nunc fringilla', 15)); // "Nunc sed turpis..."
-console.log(formatMessage('Nunc sed turpis a felis in nunc fringilla', 41)); // "Nunc sed turpis a felis in nunc fringilla"
